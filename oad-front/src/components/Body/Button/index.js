93eb0956430e6620b2835b5dd550d887a3e58e85
Igor <igormcsouza@gmail.com>
2020-05-02 21:04:00 -0300

    added web picture taken

--- oad-front/src/components/Body/Button/index.js
+++ oad-front/src/components/Body/Button/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./styles.css";
 
-const Button = () => (
-    <button id="button" onClick="funcaoParaExecutar()"> Clique Aqui! </button>
+const Button = ({onClick, text}) => (
+    <button id="button" onClick={onClick}> {text} </button>
 )
 
 export default Button
--- oad-front/src/components/Body/index.js
+++ oad-front/src/components/Body/index.js
@@ -1,18 +1,62 @@
 import React from 'react'
 import './styles.css'
 import Button from './Button'
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
-const Body = () => (
-    <div id='main-body'>
-        <div id='section'>
-            <h1> Carregar imagem </h1>
-            <Button />
-        </div>
-        <div id='section'>
-            <h1> Tirar Foto </h1>
-            <Button />
-        </div>
-    </div>
-)
+export default class Body extends React.Component{
+    state = {
+        renderCase: ''
+    }
 
-export default Body
+    handleTakePhoto (dataUri) {
+        // Do stuff with the photo...
+        console.log(dataUri);
+    }
+
+    renderOnScreen(param){
+        switch(param) {
+            case 'load':
+                return <p>LOAD</p>
+
+            case 'take':
+                return (
+                    <div id='main-body'>
+                        <div id='section'>
+                            <Camera
+                                onTakePhoto = { (dataUri) => { this.handleTakePhoto(dataUri); } }
+                            />
+                        </div>
+                    </div>
+                )
+
+            default:
+                return (
+                    <div id='main-body'>
+                        <div id='section'>
+                            <h1> Carregar imagem </h1>
+                            <Button 
+                                onClick={() => {
+                                    this.setState({renderCase: "load"})
+                                }}
+                                text='Carregar Imagens'
+                            />
+                        </div>
+                        <div id='section'>
+                            <h1> Tirar Foto </h1>
+                            <Button 
+                                onClick={() => {
+                                    this.setState({renderCase: "take"})
+                                }}
+                                text='Tirar Foto'
+                            />
+                        </div>
+                    </div>
+                )
+        }
+    }
+
+    render(){
+        return (<div>{ this.renderOnScreen(this.state.renderCase) }</div>)
+    }
+}
